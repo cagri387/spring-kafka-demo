afbbdb5fca27d72c1630a8ff45283cef6e8d5921
cagri.tras <cagri.tras@intra.local>
2020-04-11 17:01:30 +0300

    Düzeltme.

--- src/test/java/com/example/spiringkafkademo/FlightProcessor.java
+++ src/test/java/com/example/spiringkafkademo/FlightProcessor.java
@@ -1,6 +1,7 @@
 package com.example.spiringkafkademo;
 
 import com.example.spiringkafkademo.entity.Flight;
+import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,30 +9,16 @@ import org.springframework.kafka.listener.MessageListener;
 
 import java.util.concurrent.CountDownLatch;
 
+@RequiredArgsConstructor
 public class FlightProcessor implements MessageListener<Integer, Flight> {
 
 	private Logger logger = LoggerFactory.getLogger(FlightProcessor.class);
 
-	private int processed_number;
-
 	private final CountDownLatch latch;
-
-	public FlightProcessor(CountDownLatch latch) {
-		this.latch = latch;
-	}
 
 	@Override
 	public void onMessage(ConsumerRecord<Integer, Flight> data) {
 		logger.info("Test Flight message consumed: " + data.value());
 		latch.countDown();
 	}
-
-	public int getProcessed_number() {
-		return processed_number;
-	}
-
-	public void setProcessed_number(int processed_number) {
-		this.processed_number = processed_number;
-	}
-
 }
